8183a7f28d7aabd23c623d78317d062badd0d07f
Artem <33758888+Fobozzz86@users.noreply.github.com>
2023-07-03 18:16:17 +0300

    1_Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -5,8 +5,9 @@ public class Main {
 
         for (int i = 1; i <= 10; i++) {
 
-       
-            // for you, but you can always add more by pressing Ctrl+F8.
+       System.out.prinln("Hello AAAAAArtem!!!!!!!");
+            
+            // for you Ctrl+F8.
             System.out.println("i = " + i);
         }
     }
